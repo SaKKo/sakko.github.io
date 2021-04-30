88bd9900e403cb7c32170ca2ee18390e8c024ead
sakko <saklism@gmail.com>
2021-04-30 16:24:36 +0700

    finished testing phase

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+    <Topbar></Topbar>
+    <b-container class="sk-container">
+      <Nuxt></Nuxt>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped>
+.sk-container {
+  margin-top: 40px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div>
+    <b-row>
+      <b-col v-for="post in posts" :key="post.slug">
+        <b-card
+          :title="post.title"
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            {{ post.description }}
+          </b-card-text>
+          <b-button
+            :to="{ name: 'posts-slug', params: { slug: post.slug } }"
+            variant="primary"
+            >Go somewhere</b-button
+          >
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const postsData = await $content('posts').fetch();
+    return {
+      posts: postsData,
+    };
+  },
+  mounted() {
+    console.log('THIS.POSTS', this.posts);
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
